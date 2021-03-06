039584c8e40cafeb977666a0cab1bdcc2353710d
myrongreensr <79763879+myrongreensr@users.noreply.github.com>
2021-03-06 13:42:15 -0600

    Updated files for Labwork 6

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<title>iCare System Monitor</title>
+<meta charset="utf-8">
+<style>
+* { box-sizing: border-box; }
+body {font-family:Verdana, Arial, sans-serif;
+      background-color: gray;
+}
+#wrapper { background-color: blue; 
+           color: #000066;
+           width: 80%;
+		   margin: auto;
+           min-width: 960px;
+		   max-width: 1200px;
+} 
+nav { float: left;
+	  width: 150px;
+}
+nav ul { list-style-type: none;
+		margin-left: 0;
+		padding: 10px; } 
+nav a { text-decoration: none;
+		padding: 10px;
+		font-weight: bold; }
+nav a:link { color: #ffffff; }
+nav a:visited { color: #eaeaea; }
+nav a:hover { color: #000066; }		
+main {  display: block; /* for Internet Explorer links  */
+        margin-left: 155px;
+        padding: 20px;
+        background-color: #ffffff;
+        color: #000000;
+} 
+header { background-color: #869dc7;
+        color: white; 
+        font-size: 150%; 
+        padding: 10px 10px 10px 155px;
+		background-image: url(iCareflatlinelogo.jpg); 
+		background-repeat: no-repeat;
+}
+h1 { margin-bottom: 20px; }
+h2 { color: #869dc7; 
+     font-family: arial, sans-serif;
+}
+ 
+#floatright { margin: 10px;
+              float: right;
+}
+
+footer { font-size:70%;
+         text-align: center;
+		 clear: right;
+         padding: 20px;
+		 background-color: #869dc7; 
+}
+</style> 
+</head>
+<body>
+<div id="wrapper">
+  <header>
+    <h1>iCare System Monitor</h1>
+  </header>
+  <nav>
+  <ul>
+    <li><a href="index.html">Home</a></li>
+    <li><a href="menu.html">Menu</a></li>
+    <li><a href="directions.html">Directions</a></li>
+    <li><a href="contact.html">Contact</a></li>
+  </ul>
+  </nav>
+  <main>
+      <img src="iCarewatch.jpg" width="250" height="355" id="floatright" alt="iCarewatch">
+	<h2>What is iCare Monitor</h2>
+    <p>A new technology that is designed to help people keep closer tabs on their heart health, and can even be used to help identify atrial fibrillation (Afib) a serious medical condition that is a leading cause of stroke.</p>
+    <h2>Special Design</h2>
+    <p>iCare Monitor was thrust into the wearable spotlight via the Apple & Samsung Products. It's now finding it way into other smartwatches including the Samsung Galaxy Watch 3 and new Fitbit Sense health watch.</p>
+  	<h2>History of Medical Wearables</h2>
+    <p>Most of the wearables on the market at the moment have optical heart rate monitors inside them. This is a monitor that uses flashing LEDs to penetrate the skin and detect blood flow. When light is reflected off the flow of blood, it’s captured by the sensors. The algorithm then gets to work to produce heart rate data. </p>
+    <h2>From a 500ft View</h2>
+	<p>They key question for many people is 'why buy an ECG smartwatch?'. Well, for many (dare we say most people) the feature is something you'll likely use once and then forget about. But for a large subset of people, ECG is absoutely essential.
+
+“ECG on wearables is super high value for a small segment. It is probably going to be more advantageous to our users over 50 than 20 - 25 year olds, but we're trying to build something for everybody," Dr Conor Heneghan, Director of Research Algorithms at Fitbit told Wareable.</p>
+    </main>
+    <footer>Copyrighted By Myron Green Sr&copy; 2021
+    </footer>
+</div>
+</body>
+</html>
+
+
