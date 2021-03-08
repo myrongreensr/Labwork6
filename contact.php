bfd08c4893ee0662c2aad6d52c30a2b5f4b631ac
myrongreensr <79763879+myrongreensr@users.noreply.github.com>
2021-03-08 10:49:52 -0600

    Update home and contact page
    updated home and contact pages

---
+++ contact.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<title>iCare System Monitor</title>
+<meta charset="utf-8">
+<style>
+* { box-sizing: border-box; }
+body {font-family:Verdana, Arial, sans-serif;
+      background-color: gray;
+}
+#wrapper { background-color: blue; 
+           color: #000066;
+           width: 80%;
+		   margin: auto;
+           min-width: 960px;
+		   max-width: 1200px;
+} 
+nav { float: left;
+	  width: 150px;
+}
+nav ul { list-style-type: none;
+		margin-left: 0;
+		padding: 10px; } 
+nav a { text-decoration: none;
+		padding: 10px;
+		font-weight: bold; }
+nav a:link { color: #ffffff; }
+nav a:visited { color: #eaeaea; }
+nav a:hover { color: #000066; }		
+main {  display: block; /* for Internet Explorer links  */
+        margin-left: 155px;
+        padding: 20px;
+        background-color: #ffffff;
+        color: #000000;
+} 
+header { background-color: #869dc7;
+        color: white; 
+        font-size: 150%; 
+        padding: 10px 10px 10px 155px;
+		background-image: url(iCareflatlinelogo.jpg); 
+		background-repeat: no-repeat;
+}
+h1 { margin-bottom: 20px; }
+h2 { color: #869dc7; 
+     font-family: arial, sans-serif;
+}
+ 
+#floatright { margin: 10px;
+              float: right;
+}
+
+footer { font-size:70%;
+         text-align: center;
+		 clear: right;
+         padding: 20px;
+		 background-color: #869dc7; 
+}
+</style> 
+</head>
+<body>
+<div id="wrapper">
+  <header>
+    <h1>iCare System Monitor</h1>
+  </header>
+  <nav>
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="menu.php">Menu</a></li>
+    <li><a href="directions.php">Directions</a></li>
+    <li><a href="contact.php">Contact</a></li>
+  </ul>
+  </nav>
+  <img src="iCarewatch.jpg" width="250" height="355" id="floatright" alt="iCarewatch">
+  <main>
+  <h2>Our Team: </h2>
+    <p>We operate in an industry built on trust. This can only be achieved through communication and experienced support - from the first contact unitl the last.</p>
+    <h2>Questions</h2>
+    <p>If you have any questions regarding iCare Monitor System, background or screening our entire team reciees specialized training regulary to ensure you're getting the best information possible. From basic qhestions to the complex inquires, we're are here to help.</p>
+  	<h2>Interested in iCare System Montior Wearables</h2>
+    <p>Our Account Executives take the time to discuss your existing background screening program and help you make smart decisions that best meet your needs. </p>
+    <h2>Company Email</h2>
+  <li>E-mail: <a href="mailto:contact@iCareSystem.com">contact@iCareSystem.com</a></li>
+   <li>Phone: 555-555-5555</li>
+     
+
+
+
+      </main>
+    <footer>Copyrighted By Myron Green Sr&copy; 2021
+    </footer>
+</div>
+</body>
+</html>
+
+	
