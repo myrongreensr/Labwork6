13b6ad6a9aae97f09cf0a91282e705b943cb2eaa
myrongreensr <myrongreensr2002@gmail.com>
2021-04-23 11:28:31 -0500

    Update history.php
    update

--- history.php
+++ history.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <!DOCTYPE html> 
 <html lang="en">
 <head>
@@ -74,129 +72,30 @@ footer { font-size:70%;
   </nav>
   <main>
       <img src="iCarewatch.jpg" width="250" height="355" id="floatright" alt="iCarewatch">
-	<h2>What is iCare Monitor</h2>
-    <p>A new technology that is designed to help people keep closer tabs on their heart health, and can even be used to help identify atrial fibrillation (Afib) a serious medical condition that is a leading cause of stroke.</p>
-    <h2>Special Design</h2>
-    <p>iCare Monitor was thrust into the wearable spotlight via the Apple & Samsung Products. It's now finding it way into other smartwatches including the Samsung Galaxy Watch 3 and new Fitbit Sense health watch.</p>
-  	<h2>History of Medical Wearables</h2>
-    <p>Most of the wearables on the market at the moment have optical heart rate monitors inside them. This is a monitor that uses flashing LEDs to penetrate the skin and detect blood flow. When light is reflected off the flow of blood, it’s captured by the sensors. The algorithm then gets to work to produce heart rate data. </p>
-    <h2>From a 500ft View</h2>
-	<p>The key question for many people is 'why buy an smartwatch?'. Well, for many (dare we say most people) the feature is something you'll likely use once and then forget about. But for a large subset of people, iCarewatch is absoutely essential.
+	<h2>iCare Monitor</h2>
+    <p>The Proven Success of Smart AI-Based Analytics with iCarewatch. Drives Customer Satisfaction by focusing every role in any location with easy to see, proven real-time and post-interaction metrics that measure those difficult to interpret, subjective behaviors that impact customer satisfaction. It produces objective insights into agent behavior immediately, accurately, with no bias, and no human effort</p>
+    <h2>Satisfied customers Experience "Janis G."</h2>
+    <p> I have been wearing the iCare Medical Watch for a month and I am no longer fearful of being alone and suffering a bad fall with medical complications. The install was simple and the customer service is excellent.</p>
+  	<h2>Satisfied customers Experience "Daniel G."</h2>
+    <p>My grandpa has had the best experience ever with Medical Guardian! They contacted family listed on his account when he fell and my grandpa was filled with the utmost joy to see everyone there for him. iCare System, you made a family very happy.</p>
+    <h2>FAQ's</h2>
+	<p>1. Who is the medical alert monitoring service right for?
 
-“Wearables are super high value for a small segment. It is probably going to be more advantageous to our users over 50 than 20 - 25 year olds, but we're trying to build something for everybody," Dr Conor Heneghan, Director of Research Algorithms at Fitbit told Wareable.</p>
-    
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Browser Report</title>
-<meta charset="utf-8">
-</head>
-<body>
-<h1>Monthly Browser Report</h1>
-<meter value="14417" min="0" max="14417">14417</meter>14,417 Total Visits<br>
-<meter value="7000"  min="0" max="14417">7000</meter> 7,000 Firefox<br>
-<meter value="3800"  min="0" max="14417">3800</meter> 3,800 Internet Explorer<br>
-<meter value="2062"  min="0" max="14417">2062</meter> 2,062 Chrome<br>
-<meter value="1043"  min="0" max="14417">1043</meter> 1,043 Safari<br>
-<meter value="312"  min="0" max="14417">312</meter> &nbsp;&nbsp; 312 Opera<br>
-<meter value="200"  min="0" max="14417">200</meter> &nbsp;&nbsp; 200 other<br>
+Medical alert systems make sense for seniors and people of all ages that live alone or spend long periods of time alone and may require emergency medical assistance. This can include those with medical conditions such as epilepsy, stroke, seizures, heart conditions, or those with a likelihood to suffer from a fall. In the event of an emergency situation, the person can quickly contact operators to have them dispatch police, ambulance, or firefighters or alert designated caregivers and contacts.
 
-</body>
-</html>
+2. What kind of services do medical alert systems provide?
 
+In the event of a medical emergency, personal alert systems allow the user to quickly get in touch with someone to send help. The medical alert operator will be prepared with all the important contacts including family, friends, physician, etc. for effective and fast assistance in the event of an emergency. The operators can also dispatch emergency professionals such as the police, paramedics, or firefighters when necessary.
 
+3. What kind of devices do medical alert systems include?
 
-</main>
-    <footer>Copyrighted By Myron Green Sr&copy; 2021
-    </footer>
-</div>
-</body>
-</html>
+Medical alert solutions will offer a range of different devices that allow the user to instantly get help in case of a medical emergency. Medical alert systems offer different types of devices for at home or on-the-go mobile alerts.
 
-
-=======
-<!DOCTYPE html> 
-<html lang="en">
-<head>
-<title>iCare System Monitor</title>
-<meta charset="utf-8">
-<style>
-* { box-sizing: border-box; }
-body {font-family:Verdana, Arial, sans-serif;
-      background-color: gray;
-}
-#wrapper { background-color: blue; 
-           color: #000066;
-           width: 80%;
-		   margin: auto;
-           min-width: 960px;
-		   max-width: 1200px;
-} 
-nav { float: left;
-	  width: 150px;
-}
-nav ul { list-style-type: none;
-		margin-left: 0;
-		padding: 10px; } 
-nav a { text-decoration: none;
-		padding: 10px;
-		font-weight: bold; }
-nav a:link { color: #ffffff; }
-nav a:visited { color: #eaeaea; }
-nav a:hover { color: #000066; }		
-main {  display: block; /* for Internet Explorer links  */
-        margin-left: 155px;
-        padding: 20px;
-        background-color: #ffffff;
-        color: #000000;
-} 
-header { background-color: #869dc7;
-        color: white; 
-        font-size: 150%; 
-        padding: 10px 10px 10px 155px;
-		background-image: url(iCareflatlinelogo.jpg); 
-		background-repeat: no-repeat;
-}
-h1 { margin-bottom: 20px; }
-h2 { color: #869dc7; 
-     font-family: arial, sans-serif;
-}
+The device may operate by pushing a button in order to communicate with an operator. At home systems usually connect to a landline, but do not require one. For more active users, the mobile alert systems provide GPS enabled systems with unlimited range. Services may also include fall detection and other add-ons.
  
-#floatright { margin: 10px;
-              float: right;
-}
+4. What are medical alert bracelets? 
 
-footer { font-size:70%;
-         text-align: center;
-		 clear: right;
-         padding: 20px;
-		 background-color: #869dc7; 
-}
-</style> 
-</head>
-<body>
-<div id="wrapper">
-  <header>
-    <h1>iCare System Monitor</h1>
-  </header>
-  <nav>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="services.php">Services</a></li>
-    <li><a href="contact.php">Contact</a></li>
-    <li><a href="directions.php">Directions</a></li>
-  </ul>
-  </nav>
-  <main>
-      <img src="iCarewatch.jpg" width="250" height="355" id="floatright" alt="iCarewatch">
-	<h2>What is iCare Monitor</h2>
-    <p>A new technology that is designed to help people keep closer tabs on their heart health, and can even be used to help identify atrial fibrillation (Afib) a serious medical condition that is a leading cause of stroke.</p>
-    <h2>Special Design</h2>
-    <p>iCare Monitor was thrust into the wearable spotlight via the Apple & Samsung Products. It's now finding it way into other smartwatches including the Samsung Galaxy Watch 3 and new Fitbit Sense health watch.</p>
-  	<h2>History of Medical Wearables</h2>
-    <p>Most of the wearables on the market at the moment have optical heart rate monitors inside them. This is a monitor that uses flashing LEDs to penetrate the skin and detect blood flow. When light is reflected off the flow of blood, it’s captured by the sensors. The algorithm then gets to work to produce heart rate data. </p>
-    <h2>From a 500ft View</h2>
-	<p>The key question for many people is 'why buy an smartwatch?'. Well, for many (dare we say most people) the feature is something you'll likely use once and then forget about. But for a large subset of people, iCarewatch is absoutely essential.
+When it comes to medical alert equipment, carrying around a large device just isn’t a practical solution. For this reason, companies have created wearable devices that users can access at all times. Items like medical alert bracelets allow someone to wear their alert system comfortable and conveniently around their wrist. In this way, they can bring the device anywhere they go and have full protection on-the-go.The key question for many people is 'why buy an smartwatch?'. Well, for many (dare we say most people) the feature is something you'll likely use once and then forget about. But for a large subset of people, iCarewatch is absoutely essential.
 
 “Wearables are super high value for a small segment. It is probably going to be more advantageous to our users over 50 than 20 - 25 year olds, but we're trying to build something for everybody," Dr Conor Heneghan, Director of Research Algorithms at Fitbit told Wareable.</p>
     
@@ -229,120 +128,3 @@ footer { font-size:70%;
 </html>
 
 
->>>>>>> b873f23814b68945dcfe442bcf6d753d14397eea
-=======
-<!DOCTYPE html> 
-<html lang="en">
-<head>
-<title>iCare System Monitor</title>
-<meta charset="utf-8">
-<style>
-* { box-sizing: border-box; }
-body {font-family:Verdana, Arial, sans-serif;
-      background-color: gray;
-}
-#wrapper { background-color: blue; 
-           color: #000066;
-           width: 80%;
-		   margin: auto;
-           min-width: 960px;
-		   max-width: 1200px;
-} 
-nav { float: left;
-	  width: 150px;
-}
-nav ul { list-style-type: none;
-		margin-left: 0;
-		padding: 10px; } 
-nav a { text-decoration: none;
-		padding: 10px;
-		font-weight: bold; }
-nav a:link { color: #ffffff; }
-nav a:visited { color: #eaeaea; }
-nav a:hover { color: #000066; }		
-main {  display: block; /* for Internet Explorer links  */
-        margin-left: 155px;
-        padding: 20px;
-        background-color: #ffffff;
-        color: #000000;
-} 
-header { background-color: #869dc7;
-        color: white; 
-        font-size: 150%; 
-        padding: 10px 10px 10px 155px;
-		background-image: url(iCareflatlinelogo.jpg); 
-		background-repeat: no-repeat;
-}
-h1 { margin-bottom: 20px; }
-h2 { color: #869dc7; 
-     font-family: arial, sans-serif;
-}
- 
-#floatright { margin: 10px;
-              float: right;
-}
-
-footer { font-size:70%;
-         text-align: center;
-		 clear: right;
-         padding: 20px;
-		 background-color: #869dc7; 
-}
-</style> 
-</head>
-<body>
-<div id="wrapper">
-  <header>
-    <h1>iCare System Monitor</h1>
-  </header>
-  <nav>
-  <ul>
-    <li><a href="index.php">Home</a></li>
-    <li><a href="services.php">Services</a></li>
-    <li><a href="contact.php">Contact</a></li>
-    <li><a href="directions.php">Directions</a></li>
-  </ul>
-  </nav>
-  <main>
-      <img src="iCarewatch.jpg" width="250" height="355" id="floatright" alt="iCarewatch">
-	<h2>What is iCare Monitor</h2>
-    <p>A new technology that is designed to help people keep closer tabs on their heart health, and can even be used to help identify atrial fibrillation (Afib) a serious medical condition that is a leading cause of stroke.</p>
-    <h2>Special Design</h2>
-    <p>iCare Monitor was thrust into the wearable spotlight via the Apple & Samsung Products. It's now finding it way into other smartwatches including the Samsung Galaxy Watch 3 and new Fitbit Sense health watch.</p>
-  	<h2>History of Medical Wearables</h2>
-    <p>Most of the wearables on the market at the moment have optical heart rate monitors inside them. This is a monitor that uses flashing LEDs to penetrate the skin and detect blood flow. When light is reflected off the flow of blood, it’s captured by the sensors. The algorithm then gets to work to produce heart rate data. </p>
-    <h2>From a 500ft View</h2>
-	<p>The key question for many people is 'why buy an smartwatch?'. Well, for many (dare we say most people) the feature is something you'll likely use once and then forget about. But for a large subset of people, iCarewatch is absoutely essential.
-
-“Wearables are super high value for a small segment. It is probably going to be more advantageous to our users over 50 than 20 - 25 year olds, but we're trying to build something for everybody," Dr Conor Heneghan, Director of Research Algorithms at Fitbit told Wareable.</p>
-    
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Browser Report</title>
-<meta charset="utf-8">
-</head>
-<body>
-<h1>Monthly Browser Report</h1>
-<meter value="14417" min="0" max="14417">14417</meter>14,417 Total Visits<br>
-<meter value="7000"  min="0" max="14417">7000</meter> 7,000 Firefox<br>
-<meter value="3800"  min="0" max="14417">3800</meter> 3,800 Internet Explorer<br>
-<meter value="2062"  min="0" max="14417">2062</meter> 2,062 Chrome<br>
-<meter value="1043"  min="0" max="14417">1043</meter> 1,043 Safari<br>
-<meter value="312"  min="0" max="14417">312</meter> &nbsp;&nbsp; 312 Opera<br>
-<meter value="200"  min="0" max="14417">200</meter> &nbsp;&nbsp; 200 other<br>
-
-</body>
-</html>
-
-
-
-</main>
-    <footer>Copyrighted By Myron Green Sr&copy; 2021
-    </footer>
-</div>
-</body>
-</html>
-
-
->>>>>>> 0a5e16a05b7b80de7ecb139fdc8edc8d7d514c6b
