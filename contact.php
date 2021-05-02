40d3f512e52341355eb7792068c39a6e6751634a
myrongreensr <myrongreensr2002@gmail.com>
2021-05-02 12:55:11 -0500

    testing image on web page
    testing image on webpage to make sure it looks correct

--- contact.php
+++ contact.php
@@ -5,11 +5,11 @@
 <meta charset="utf-8">
 </head>
 <body>
-<h1>iCare System Monitor</h1>
+<h1></h1>
 <script type="text/javascript">
   alert("Welcome to the iCare System Montior where we help you own your health along with owning future!");
 </script>
-<h2>When does this display?</h2>
+
 </body>
 </html>
 
