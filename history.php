da6642ed6ce4bfa940e3321265f44a5b9994eba6
myrongreensr <myrongreensr2002@gmail.com>
2021-05-02 12:58:32 -0500

    updating document write
    updating document write on history page to keep info updated and letting customer know when its last modifed.

--- history.php
+++ history.php
@@ -118,6 +118,21 @@ The device may operate by pushing a button in order to communicate with an opera
 </body>
 </html>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>JavaScript Practice</title>
+<meta charset="utf-8">
+</head>
+<body>
+
+</script>
+
+<h3>This document was last modified on: 
+<script type="text/javascript">
+<!--
+document.write(document.lastModified);
+// -->
 
 
 </main>
